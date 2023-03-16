77c22adf749ae40502b3a791412e313d13293422
jamesv11 <jvanstrahlen@unicesar.edu.co>
2023-03-15 21:55:02 -0500

    feature: configuracion arquitectura

---
+++ applications/app-service/src/main/java/co/com/microserviceuser/MainApplication.java
@@ -0,0 +1,11 @@
+package co.com.microserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ applications/app-service/src/main/java/co/com/microserviceuser/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.microserviceuser.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.microserviceuser.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
